83da8ce63e7750fd2a7cd189ece831e8899fa458
Tian535395302 <tianzongbing@gmail.com>
2014-12-21 19:23:26 +0800

    用法说明，描述

---
+++ readme.txt
@@ -0,0 +1,25 @@
+
+使用前需要配置：
+    generator.xml
+        1) basepackage   -->    必须，所有生成java文件的基础包名。引用方式：${basepackage}
+        2) namespace     -->    可选，若不填，就不要引用。引用方式：${namespace}
+
+        3) outRoot       -->    必须，输出目录，因为MacOS和WindowOS目录表示方式不同，自己多试试。
+                                Mac OS ： <entry key="outRoot">generator-class-output</entry>
+                                结果：在项目根目录下生成generator-class-output文件夹，包含生成的文件
+                                Window OS：原项目是<entry key="outRoot">.\generator-output</entry>。
+                                结果：我没试过....
+
+        4) 数据库配置
+            jdbc.username
+            jdbc.password
+            jdbc.url
+            jdbc.driver
+
+
+    Main.main():
+        根据你的调用方式，填入参数，选择调用的方法
+
+        1）g.generateByClass(Class<?> clazz,String templateDirByClass);
+
+        2）g.generateByTable(String tableName,String templateDirByTable);
--- src/Main.java
+++ src/Main.java
@@ -25,30 +25,49 @@ public class Main {
      * gg : 模板控制变量, cn.org.rapid_framework.generator.GeneratorControl
      */
     public static void main(String... args) throws Exception {
+        /**
+         * TODO 注意：
+         * 1）clazz类的package需要含有".",不然会报一个字符串数组越界异常。因为有句代码是要以最后一个点号为offset切割类的包名。
+         * 2）第一个属性成员变量被当做PK使用，在Service，Dao中被用于ByPK为后缀方法的参数
+         * 3）serialVersionUID应当被注释，不然在ibatis的mapper文件中会被用于sql语句。
+         * 4）...
+         */
         Class clazz = CitizenInfo.class;
 
-//        Field[] fields = clazz.getDeclaredFields();
-//        System.err.println("Class::: "+clazz.getName());
-//        for (Field field : fields) {
-//            System.err.println("Field:::  "+field.getType()+"  "+field.getName());
-//
-//            Annotation[] ans = field.getDeclaredAnnotations();
-//
-//            for (Annotation an : ans) {
-//                System.err.println("    Annotation:::  "+an.getClass());
-//            }
-//        }
         GeneratorFacade g = new GeneratorFacade();
         g.deleteOutRootDir();							//删除生成器的输出目录
-        g.generateByClass(CitizenInfo.class,"template_byClass");
-        //g.printAllTableNames();				//打印数据库中的表名称
-        //g.generateByTable("user_info","template_byTable");	//通过数据库表生成文件,template为模板的根目录
-        //g.generateByAllTable("template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
 
+        /* 通过类生成文件
+         * 参数1：类
+         * 参数2：模板的根目录
+         */
+        g.generateByClass(CitizenInfo.class,"template_byClass");
+
+        //g.printAllTableNames();				//打印数据库中的表名称
+
+        /**
+         * 参数1：数据表名
+         * 参数2：模板的根目录
+         * use test;
+         * drop table if exists `user_info`;
+         * CREATE TABLE `user_info` (
+         *   `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+         *   `username` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名',
+         *   `password` varchar(50) DEFAULT NULL COMMENT '密码',
+         *   `birth_date` date DEFAULT NULL,
+         *   `sex` int(11) DEFAULT NULL,
+         *   `age` int(11) DEFAULT NULL,
+         *   PRIMARY KEY (`user_id`)
+         * ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+         */
+        g.generateByTable("user_info","template_byTable");	//通过数据库表生成文件
+
+        //其他方式
+        //g.generateByAllTable("template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
         //g.deleteByTable("table_name", "template"); //删除生成的文件
 
-       // String outRoot = GeneratorProperties.getProperty("outRoot");
-
+        // 打开文件夹
+        // String outRoot = GeneratorProperties.getProperty("outRoot");
         // Mac OS: 打开文件夹
         //Runtime.getRuntime().exec("open " + new File(outRoot).getAbsolutePath());
         // Window OS:打开文件夹
